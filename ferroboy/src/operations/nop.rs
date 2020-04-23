2520950d475b52de88d9167cb72cf7e618095ba1
berwyn <berwyn.codeweaver@gmail.com>
2020-04-23 09:33:03 -0400

    docs: Document NOP

--- ferroboy/src/operations/nop.rs
+++ ferroboy/src/operations/nop.rs
@@ -1,6 +1,27 @@
 use crate::operations::Operation;
 use crate::State;
 
+/// A non-operation.
+///
+/// # Opcode Reference
+/// ## Assembly definition
+/// ```a
+/// NOP
+/// ```
+///
+/// ## Runtime
+/// | Metric | Size |
+/// |:-------|:-----|
+/// | Length | 1    |
+/// | Cycles | 4    |
+///
+/// ## Flags
+/// | Flag          | Value        |
+/// |:--------------|:-------------|
+/// | Zero          | Not Affected |
+/// | Subtraction   | Not Affected |
+/// | Half-Carry    | Not Affected |
+/// | Carry         | Not Affected |
 #[derive(Debug)]
 pub struct NopOperation;
 
