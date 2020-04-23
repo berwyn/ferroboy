0418a6cfadade8d8bf097ac8a51321294be19d16
berwyn <berwyn.codeweaver@gmail.com>
2020-04-23 09:14:12 -0400

    docs: Document the load8 family of operations

--- ferroboy/src/operations/load8.rs
+++ ferroboy/src/operations/load8.rs
@@ -1,10 +1,43 @@
+use crate::assembly::{AssemblyInstruction, AssemblyInstructionBuilder};
 use crate::helpers::word_to_u16;
 use crate::operations::Operation;
 use crate::state::State;
 use crate::system::Register;
 
+// ? Should this be split up into separate files?
+
+/// Loads an immediate 8-bit value into a register.
+///
+/// # Opcode Reference
+/// ## Assembly definition
+/// ```a
+/// LD A,#
+/// ```
+///
+/// ## Runtime
+/// | Metric | Size |
+/// |:-------|:-----|
+/// | Length | 2    |
+/// | Cycles | 8    |
+///
+/// ## Flags
+/// | Flag          | Value        |
+/// |:--------------|:-------------|
+/// | Zero          | Not Affected |
+/// | Subtraction   | Not Affected |
+/// | Half-Carry    | Not Affected |
+/// | Carry         | Not Affected |
+///
+/// # Examples
+/// ```rs
+/// Load8ImmediateOperation(Register::A)
+/// ```
+///
+/// # Errors
+/// - The operation may fail if provided a 16-bit register
 #[derive(Debug)]
 pub struct Load8ImmediateOperation(pub Register);
+// FIXME: Register refactor
 
 impl Operation for Load8ImmediateOperation {
     fn act(&self, state: &mut State) -> crate::Result<()> {
@@ -15,8 +48,40 @@ impl Operation for Load8ImmediateOperation {
     }
 }
 
+// TODO: Implement Disassemble
+
+/// Copy the value of one register to another.
+///
+/// # Opcode Reference
+/// ## Assembly definition
+/// ```a
+/// LD A,B
+/// ```
+///
+/// ## Runtime
+/// | Metric | Size |
+/// |:-------|:-----|
+/// | Length | 1    |
+/// | Cycles | 4    |
+///
+/// ## Flags
+/// | Flag          | Value        |
+/// |:--------------|:-------------|
+/// | Zero          | Not Affected |
+/// | Subtraction   | Not Affected |
+/// | Half-Carry    | Not Affected |
+/// | Carry         | Not Affected |
+///
+/// # Examples
+/// ```rs
+/// Load8RegisterCopyOperation(Register::A, Register::B)
+/// ```
+///
+/// # Errors
+/// - The operation may fail if provided a 16-bit register
 #[derive(Debug)]
 pub struct Load8RegisterCopyOperation(pub Register, pub Register);
+// FIXME: Register refactor
 
 impl Operation for Load8RegisterCopyOperation {
     fn act(&self, state: &mut State) -> crate::Result<()> {
@@ -27,6 +92,51 @@ impl Operation for Load8RegisterCopyOperation {
     }
 }
 
+impl core::convert::TryFrom<Load8RegisterCopyOperation> for AssemblyInstruction {
+    type Error = String;
+
+    fn try_from(value: Load8RegisterCopyOperation) -> Result<Self, Self::Error> {
+        AssemblyInstructionBuilder::new()
+            .with_command("LD")
+            .with_arg(value.0)
+            .with_arg(value.1)
+            .build()
+    }
+}
+
+// FIXME: This doesn't work for LD A,(C), LD A,(a8) or LD A,(a16)
+// ? Maybe a flag enum? Load8MemorySource with values Register, WideRegister, Immediate, WideImmediate?
+// FIXME: Metrics here are based as-implemented, but should be updated when impl is correct
+// e.g. LD A,(a16) is 3 bytes, 16 cycles vs LD A,(HL) at 1 byte 8 cycles
+/// Load an 8-bit value from the address stored in a 16-bit register.
+///
+/// # Opcode Reference
+/// ## Assembly definition
+/// ```a
+/// LD A,(HL)
+/// ```
+///
+/// ## Runtime
+/// | Metric | Size |
+/// |:-------|:-----|
+/// | Length | 1    |
+/// | Cycles | 8    |
+///
+/// ## Flags
+/// | Flag          | Value        |
+/// |:--------------|:-------------|
+/// | Zero          | Not Affected |
+/// | Subtraction   | Not Affected |
+/// | Half-Carry    | Not Affected |
+/// | Carry         | Not Affected |
+///
+/// # Examples
+/// ```rs
+/// Load8RegisterCopyOperation(Register::A, Register::B)
+/// ```
+///
+/// # Errors
+/// - The operation may fail if the first argument is a 16-bit register
 #[derive(Debug)]
 pub struct Load8FromMemoryOperation(pub Register, pub Register);
 
@@ -45,6 +155,44 @@ impl Operation for Load8FromMemoryOperation {
     }
 }
 
+// TODO: LD (HL),d8 (0x36)
+// TODO: LD (a16),SP (0x08)
+// TODO: LD (HL+,A) / LD (HL-),A
+// ? What are the plus and minus?
+
+// FIXME: Metrics here are based on implementation and should be fixed
+// FIXME: Doesn't support LD (a16),A
+/// Copies a value from a register to the address held in a register.
+///
+/// # Opcode Reference
+/// ## Assembly definition
+/// ```a
+/// LD (HL),A
+/// ```
+///
+/// ## Runtime
+/// | Metric | Size |
+/// |:-------|:-----|
+/// | Length | 1*   |
+/// | Cycles | 8*   |
+///
+/// `*`: `LD (C),A` (0xE2) is 2 bytes and 8 cycles.
+///
+/// ## Flags
+/// | Flag          | Value        |
+/// |:--------------|:-------------|
+/// | Zero          | Not Affected |
+/// | Subtraction   | Not Affected |
+/// | Half-Carry    | Not Affected |
+/// | Carry         | Not Affected |
+///
+/// # Examples
+/// ```rs
+/// Load8RegisterToMemoryOperation(Register::HL, Register::A)
+/// ```
+///
+/// # Errors
+/// - The operation may fail if a 16-bit register is provided as the source
 #[derive(Debug)]
 pub struct Load8RegisterToMemoryOperation(pub Register, pub Register);
 
