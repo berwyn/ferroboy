7b509682913242daed38fb89f1319291382ae97f
berwyn <berwyn.codeweaver@gmail.com>
2020-04-23 09:33:16 -0400

    docs: Document Operation

--- ferroboy/src/operations/operation.rs
+++ ferroboy/src/operations/operation.rs
@@ -1,5 +1,11 @@
 use crate::state::State;
 
+/// An action that can be taken on the system.
+///
+/// Operations should, ideally, encapsulate all the changes
+/// to the system that occur as the result of an instruction.
+/// This includes things like mutating registers, changing
+/// flags, writing to memory, etc.
 pub trait Operation: Sync + std::fmt::Debug {
     fn act(&self, state: &mut State) -> crate::Result<()>;
 }
