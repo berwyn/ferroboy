d174cd69802f8fe642c9c3eb7803bd564a9a0c02
berwyn <berwyn.codeweaver@gmail.com>
2020-06-17 14:52:34 -0400

    refactor(ferroboy): Clean up instruction, add size

--- ferroboy/src/assembly/instruction.rs
+++ ferroboy/src/assembly/instruction.rs
@@ -5,6 +5,7 @@ pub struct AssemblyInstruction {
     command: String,
     args: [Option<String>; 2],
     comment: Option<String>,
+    size: u8,
 }
 
 impl std::fmt::Display for AssemblyInstruction {
@@ -27,80 +28,11 @@ impl std::fmt::Display for AssemblyInstruction {
     }
 }
 
-#[cfg(test)]
-mod assembly_instruction_display_tests {
-    use super::*;
-
-    #[test]
-    fn it_formats_noarg_instructions() {
-        let instruction = AssemblyInstruction {
-            command: String::from("TEST"),
-            args: [None, None],
-            comment: None,
-        };
-
-        assert_eq!("TEST", instruction.to_string());
-    }
-
-    #[test]
-    fn it_formats_single_argument_instructions() {
-        let instruction = AssemblyInstruction {
-            command: String::from("TEST"),
-            args: [Some(String::from("A")), None],
-            comment: None,
-        };
-
-        assert_eq!("TEST A", instruction.to_string());
-    }
-
-    #[test]
-    fn it_formats_dual_argument_instructions() {
-        let instruction = AssemblyInstruction {
-            command: String::from("TEST"),
-            args: [Some(String::from("A")), Some(String::from("B"))],
-            comment: None,
-        };
-
-        assert_eq!("TEST A,B", instruction.to_string());
-    }
-
-    #[test]
-    fn it_formats_comments_correctly() {
-        let command = String::from("TEST");
-        let lhs = Some(String::from("LEFT"));
-        let rhs = Some(String::from("RIGHT"));
-        let comment = Some(String::from("Distilled wisdom"));
-
-        let noop = AssemblyInstruction {
-            command: command.clone(),
-            args: [None, None],
-            comment: comment.clone(),
-        };
-
-        assert_eq!("TEST ; Distilled wisdom", noop.to_string());
-
-        let single_arg = AssemblyInstruction {
-            command: command.clone(),
-            args: [lhs.clone(), None],
-            comment: comment.clone(),
-        };
-
-        assert_eq!("TEST LEFT ; Distilled wisdom", single_arg.to_string());
-
-        let dual_arg = AssemblyInstruction {
-            command,
-            args: [lhs, rhs],
-            comment,
-        };
-
-        assert_eq!("TEST LEFT,RIGHT ; Distilled wisdom", dual_arg.to_string());
-    }
-}
-
 pub(crate) struct AssemblyInstructionBuilder {
     command: Option<String>,
     args: [Option<String>; 2],
     comment: Option<String>,
+    size: u8,
 }
 
 impl AssemblyInstructionBuilder {
@@ -109,6 +41,7 @@ impl AssemblyInstructionBuilder {
             command: None,
             args: [None, None],
             comment: None,
+            size: 1,
         }
     }
 
@@ -127,9 +60,8 @@ impl AssemblyInstructionBuilder {
         self
     }
 
-    #[allow(unused)]
-    pub fn with_comment(mut self, arg: impl ToString) -> Self {
-        self.comment.replace(arg.to_string());
+    pub fn with_size(mut self, size: u8) -> Self {
+        self.size = size;
         self
     }
 
@@ -142,71 +74,155 @@ impl AssemblyInstructionBuilder {
             command: self.command.unwrap(),
             args: self.args,
             comment: self.comment,
+            size: self.size,
         })
     }
 }
 
 #[cfg(test)]
-mod assembly_instruction_builder_tests {
+mod tests {
     use super::*;
 
-    #[test]
-    fn assembly_instruction_builder_allows_no_args() {
-        let res = AssemblyInstructionBuilder::new()
-            .with_command("jmp")
-            .build();
+    mod assembly_instruction {
+        use super::*;
 
-        assert!(res.is_ok());
-        assert_eq!(res.unwrap().command, String::from("jmp"));
+        mod display {
+            use super::*;
+
+            #[test]
+            fn it_formats_noarg_instructions() {
+                let instruction = AssemblyInstruction {
+                    command: String::from("TEST"),
+                    args: [None, None],
+                    comment: None,
+                    size: 1,
+                };
+
+                assert_eq!("TEST", instruction.to_string());
+            }
+
+            #[test]
+            fn it_formats_single_argument_instructions() {
+                let instruction = AssemblyInstruction {
+                    command: String::from("TEST"),
+                    args: [Some(String::from("A")), None],
+                    comment: None,
+                    size: 1,
+                };
+
+                assert_eq!("TEST A", instruction.to_string());
+            }
+
+            #[test]
+            fn it_formats_dual_argument_instructions() {
+                let instruction = AssemblyInstruction {
+                    command: String::from("TEST"),
+                    args: [Some(String::from("A")), Some(String::from("B"))],
+                    comment: None,
+                    size: 1,
+                };
+
+                assert_eq!("TEST A,B", instruction.to_string());
+            }
+
+            #[test]
+            fn it_formats_comments_correctly() {
+                let command = String::from("TEST");
+                let lhs = Some(String::from("LEFT"));
+                let rhs = Some(String::from("RIGHT"));
+                let comment = Some(String::from("Distilled wisdom"));
+
+                let noop = AssemblyInstruction {
+                    command: command.clone(),
+                    args: [None, None],
+                    comment: comment.clone(),
+                    size: 1,
+                };
+
+                assert_eq!("TEST ; Distilled wisdom", noop.to_string());
+
+                let single_arg = AssemblyInstruction {
+                    command: command.clone(),
+                    args: [lhs.clone(), None],
+                    comment: comment.clone(),
+                    size: 1,
+                };
+
+                assert_eq!("TEST LEFT ; Distilled wisdom", single_arg.to_string());
+
+                let dual_arg = AssemblyInstruction {
+                    command,
+                    args: [lhs, rhs],
+                    comment,
+                    size: 1,
+                };
+
+                assert_eq!("TEST LEFT,RIGHT ; Distilled wisdom", dual_arg.to_string());
+            }
+        }
     }
 
-    #[test]
-    fn assembly_instruction_builder_allows_one_arg() {
-        let res = AssemblyInstructionBuilder::new()
-            .with_command("jmp")
-            .with_arg("$100")
-            .build();
+    mod assembly_instruction_builder {
+        use super::*;
 
-        assert!(res.is_ok());
+        #[test]
+        fn assembly_instruction_builder_allows_no_args() {
+            let res = AssemblyInstructionBuilder::new()
+                .with_command("jmp")
+                .build();
 
-        let instruction = res.unwrap();
+            assert!(res.is_ok());
+            assert_eq!(res.unwrap().command, String::from("jmp"));
+        }
 
-        assert_eq!(instruction.command, String::from("jmp"));
-        assert_eq!(instruction.args, [Some(String::from("$100")), None]);
-    }
+        #[test]
+        fn assembly_instruction_builder_allows_one_arg() {
+            let res = AssemblyInstructionBuilder::new()
+                .with_command("jmp")
+                .with_arg("$100")
+                .build();
 
-    #[test]
-    fn assembly_instruction_builder_allows_two_args() {
-        let res = AssemblyInstructionBuilder::new()
-            .with_command("ld")
-            .with_arg("(A)")
-            .with_arg("$100")
-            .build();
+            assert!(res.is_ok());
 
-        assert!(res.is_ok());
+            let instruction = res.unwrap();
 
-        let instruction = res.unwrap();
+            assert_eq!(instruction.command, String::from("jmp"));
+            assert_eq!(instruction.args, [Some(String::from("$100")), None]);
+        }
 
-        assert_eq!(instruction.command, String::from("ld"));
-        assert_eq!(instruction.args[0], Some(String::from("(A)")));
-        assert_eq!(instruction.args[1], Some(String::from("$100")));
-    }
+        #[test]
+        fn assembly_instruction_builder_allows_two_args() {
+            let res = AssemblyInstructionBuilder::new()
+                .with_command("ld")
+                .with_arg("(A)")
+                .with_arg("$100")
+                .build();
 
-    #[test]
-    fn assembly_instruction_builder_ignores_excess_args() {
-        let res = AssemblyInstructionBuilder::new()
-            .with_command("ld")
-            .with_arg("A")
-            .with_arg("B")
-            .with_arg("C")
-            .build();
+            assert!(res.is_ok());
 
-        assert!(res.is_ok());
+            let instruction = res.unwrap();
 
-        let instruction = res.unwrap();
+            assert_eq!(instruction.command, String::from("ld"));
+            assert_eq!(instruction.args[0], Some(String::from("(A)")));
+            assert_eq!(instruction.args[1], Some(String::from("$100")));
+        }
 
-        assert_eq!(instruction.command, String::from("ld"));
-        assert_eq!(instruction.args[0], Some(String::from("A")));
-        assert_eq!(instruction.args[1], Some(String::from("B")));
+        #[test]
+        fn assembly_instruction_builder_ignores_excess_args() {
+            let res = AssemblyInstructionBuilder::new()
+                .with_command("ld")
+                .with_arg("A")
+                .with_arg("B")
+                .with_arg("C")
+                .build();
+
+            assert!(res.is_ok());
+
+            let instruction = res.unwrap();
+
+            assert_eq!(instruction.command, String::from("ld"));
+            assert_eq!(instruction.args[0], Some(String::from("A")));
+            assert_eq!(instruction.args[1], Some(String::from("B")));
+        }
     }
 }
