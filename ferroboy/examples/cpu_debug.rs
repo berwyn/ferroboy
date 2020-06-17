b2f1ac03b77e482b85dce549580eb8953bee5b32
berwyn <berwyn.codeweaver@gmail.com>
2020-06-17 14:51:11 -0400

    refactor(ferroboy): Move test binstubs to examples

--- ferroboy/examples/cpu_debug.rs
+++ ferroboy/examples/cpu_debug.rs
@@ -6,7 +6,7 @@ use ferroboy::StateBuilder;
 pub fn main() {
     println!("ferroboy v{}", env!("CARGO_PKG_VERSION"));
 
-    let buf = include_bytes!("../../assets/gb-test-roms/cpu_instrs/cpu_instrs.gb");
+    let buf = include_bytes!("../assets/gb-test-roms/cpu_instrs/cpu_instrs.gb");
     let config = ConfigBuilder::new().without_boot_check().build();
     let mut state = StateBuilder::new().with_config(config).build();
 
