b6d9e326c751a9ce3502f14461540096edaacec7
berwyn <berwyn@users.noreply.github.com>
2021-02-22 18:04:29 +0000

    deploy: 8e38580c55a27cac36ebc4dfa1e8341f61067a77

--- implementors/core/ops/drop/trait.Drop.js
+++ implementors/core/ops/drop/trait.Drop.js
@@ -1,6 +1,7 @@
 (function() {var implementors = {};
 implementors["backtrace"] = [{"text":"impl Drop for BacktraceFrameFmt&lt;'_, '_, '_&gt;","synthetic":false,"types":[]}];
 implementors["eyre"] = [{"text":"impl Drop for Report","synthetic":false,"types":[]}];
+implementors["once_cell"] = [{"text":"impl&lt;T&gt; Drop for OnceBox&lt;T&gt;","synthetic":false,"types":[]}];
 implementors["sharded_slab"] = [{"text":"impl&lt;'a, T, C&gt; Drop for PoolGuard&lt;'a, T, C&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: Clear + Default,<br>&nbsp;&nbsp;&nbsp;&nbsp;C: Config,&nbsp;</span>","synthetic":false,"types":[]},{"text":"impl&lt;'a, T, C:&nbsp;Config&gt; Drop for Guard&lt;'a, T, C&gt;","synthetic":false,"types":[]}];
 implementors["syn"] = [{"text":"impl&lt;'a&gt; Drop for ParseBuffer&lt;'a&gt;","synthetic":false,"types":[]}];
 implementors["thread_local"] = [{"text":"impl&lt;T:&nbsp;Send&gt; Drop for ThreadLocal&lt;T&gt;","synthetic":false,"types":[]}];
